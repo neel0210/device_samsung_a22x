5d2936a67acf9e703319404a14527ced76587f02
Neel <neeljadhavforcoc@gmail.com>
2024-12-05 20:16:51 -0500

    a22x: fix fastbootd

--- device.mk
+++ device.mk
@@ -6,3 +6,13 @@
 #
 
 LOCAL_PATH := device/samsung/a22x
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+#
